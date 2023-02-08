d90860709faf26d261a06989ce26b5fc85f902f7
jinilpatel3108 <jinil.patel@techholding.co>
2023-02-08 15:05:54 +0530

    (fix)! : ViewModel LiveData Implemented.

--- app/src/main/java/com/example/newsapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/search/SearchFragment.kt
@@ -4,10 +4,11 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.SingleNews
@@ -15,21 +16,16 @@ import com.example.newsapp.data.NewsAdapter
 import com.example.newsapp.data.NewsItemClicked
 import com.example.newsapp.databinding.FragmentSearchBinding
 import com.example.newsapp.model.News
-import com.example.newsapp.model.Response
-import com.example.newsapp.ui.saved.SavedFragment
-import com.example.newsapp.utils.APIInterface
-import com.example.newsapp.utils.ApiClient
-import com.example.newsapp.utils.Utils
-import retrofit2.Call
-import retrofit2.Callback
 
 class SearchFragment : Fragment() , NewsItemClicked{
 
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
     private lateinit var mAdapter: NewsAdapter
-
+    private lateinit var searchViewModel: SearchViewModel
     private lateinit var searchView: SearchView
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -41,22 +37,35 @@ class SearchFragment : Fragment() , NewsItemClicked{
         val recyclerView: RecyclerView = binding.newsRecyclerSearched
         searchView = binding.searchBar
 
+        searchViewModel.searchNews.observe(viewLifecycleOwner, Observer { list ->
+            list?.let {
+                mAdapter.updateNews(it)
+            }
+        })
+
+        searchViewModel.queryString.observe(viewLifecycleOwner, Observer {
+            searchViewModel.fetchData()
+        })
+
         recyclerViewAdapter(recyclerView)
         searchViewMethod(searchView)
 
         return root
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun recyclerViewAdapter(recyclerView: RecyclerView){
         recyclerView.layoutManager = LinearLayoutManager(context)
         mAdapter = NewsAdapter(this)
         recyclerView.adapter =mAdapter
+        searchViewModel.fetchData()
     }
 
     private fun searchViewMethod(searchView: SearchView){
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             @RequiresApi(Build.VERSION_CODES.O)
             override fun onQueryTextSubmit(query: String?): Boolean {
-                fetchData(query)
+                searchViewModel.queryString.postValue(query)
                 return false
             }
 
@@ -64,30 +73,12 @@ class SearchFragment : Fragment() , NewsItemClicked{
             override fun onQueryTextChange(newText: String?): Boolean {
                 return false
             }
-
         })
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun fetchData(query: String?) {
-        val apiService: APIInterface = ApiClient.getClient().create(APIInterface::class.java)
-        val call : Call<Response>? = query?.let { apiService.getSearchedNews(it, Utils.API_KEY) }
-
-        call?.enqueue(object: Callback<Response> {
-            override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
-
-                if(response.body()?.status.equals("ok")) {
-                    val articleList : List<News> = response.body()!!.news
-                    if(articleList.isNotEmpty()) {
-                        mAdapter.updateNews(articleList)
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<Response>, t: Throwable) {
-                Toast.makeText(context, "News Can't be Fetched!!", Toast.LENGTH_SHORT).show()
-            }
-        })
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        searchViewModel= ViewModelProvider(this)[SearchViewModel::class.java]
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/newsapp/ui/search/SearchViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.newsapp.ui.search
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.newsapp.model.News
+import com.example.newsapp.model.Response
+import com.example.newsapp.utils.APIInterface
+import com.example.newsapp.utils.ApiClient
+import com.example.newsapp.utils.Utils
+import retrofit2.Call
+import retrofit2.Callback
+
+class SearchViewModel(): ViewModel() {
+
+    var searchNews : MutableLiveData<List<News>> = MutableLiveData<List<News>>()
+    var queryString: MutableLiveData<String> = MutableLiveData<String>()
+
+    init {
+        fetchData()
+    }
+
+    fun fetchData(){
+        val apiService: APIInterface = ApiClient.getClient().create(APIInterface::class.java)
+        val call : Call<Response>? = queryString?.let { it.value?.let { it1 ->
+            apiService.getSearchedNews(
+                it1, Utils.API_KEY)
+        } }
+
+        call?.enqueue(object: Callback<Response> {
+            override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
+
+                if(response.body()?.status.equals("ok")) {
+                    val articleList : List<News> = response.body()!!.news
+                    if(articleList.isNotEmpty()) {
+                        searchNews.apply { postValue(articleList) }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Response>, t: Throwable) {
+
+            }
+        })
+    }
+}
